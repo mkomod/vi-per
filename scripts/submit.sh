f9cdb490a9b70c88398007ad67806ce38933986c
mkomod <mkomodromos01@gmail.com>
2023-11-10 16:14:38 +0000

    up pbs script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../src
+# cd ../src
 
 for dgp in {0..2} 
 do
@@ -9,7 +9,7 @@ do
         for P in {0..2}
         do
             # python _03_simulations.py --dgp $dgp --n $N --p $P
-            qsub -v "dgp=$dgp, P=$P, N=$N" simulations.pbs
+	    qsub -v "dgp=$dgp, P=$P, N=$N" simulations.pbs
         done
     done
 done
