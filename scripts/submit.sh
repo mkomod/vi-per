d731b1e5edfe81f9744ac60b5d50049343c01b99
mkomod <mkomodromos01@gmail.com>
2023-11-14 12:31:38 +0000

    tidy

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -9,7 +9,7 @@ do
         for P in {0..2}
         do
             # python _03_simulations.py --dgp $dgp --n $N --p $P
-	    qsub -v "dgp=$dgp, P=$P, N=$N" simulations.pbs
+            qsub -v "dgp=$dgp, P=$P, N=$N" simulations.pbs
         done
     done
 done
