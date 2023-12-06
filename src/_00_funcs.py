f6a64f236e3df0430cea49a5773855574c3e01b3
mkomod <mkomodromos01@gmail.com>
2023-12-06 15:03:02 +0000

    up src

--- src/_00_funcs.py
+++ src/_00_funcs.py
@@ -1,7 +1,23 @@
+import math
+import sys
+
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import dsdl
+import gpytorch
+import tqdm
 
 from torcheval import metrics as tm
 import torch.distributions as dist
+
+from torcheval.metrics import BinaryAUROC
+from joblib import Parallel, delayed
+
+
+from _97_gpytorch import LogisticGPVI, GPModel, LogitLikelihood, PGLikelihood
+
+
 
 def evaluate_method(fit, dat, method="vi"):
     auc = tm.BinaryAUROC()
@@ -20,3 +36,78 @@ def evaluate_method(fit, dat, method="vi"):
     cred_size = torch.mean(torch.diff(creds))
 
     return mse.compute().item(), auc.compute().item(), cov, cred_size, fit.runtime
+
+
+def process_dataset(dataset_name, standardize=True):
+    data = dsdl.load(dataset_name)
+    X, y = data.get_train()
+    X = X.todense()
+
+    X = torch.tensor(X, dtype=torch.float)
+    y = torch.tensor(y, dtype=torch.float)
+
+    if standardize:
+        Xmean = X.mean(dim=0)
+        Xstd = X.std(dim=0)
+        X = (X - Xmean) / Xstd
+
+    # ensure two classes are 0 and 1
+    classes = torch.unique(y)
+    y[y == classes[0]] = 0
+    y[y == classes[1]] = 1
+
+    X_test, y_test = data.get_test()
+    
+    if X_test is None:
+        X_test = X
+        y_test = y
+    else:
+        X_test = X_test.todense()
+        X_test = torch.tensor(X_test, dtype=torch.float)
+        y_test = torch.tensor(y_test, dtype=torch.float)
+
+        if standardize:
+            X_test = (X_test - Xmean) / Xstd
+
+        y_test[y_test == classes[0]] = 0
+        y_test[y_test == classes[1]] = 1
+
+    if X_test.size()[0] > 5000:
+        # randomly select 5000 test points
+        idx = torch.randperm(X_test.size()[0])
+        X_test = X_test[idx]
+        y_test = y_test[idx]
+    
+    return y, X, y_test, X_test
+
+
+def analyze_dataset(seed, y, X, y_test, X_test, n_iter=200, n_inducing=50, thresh=1e-6,
+                 verbose=False, use_loader=False, batches=20, standardize=True):
+    torch.manual_seed(seed)
+    print(f"Run: {seed}")
+        
+    f0 = LogisticGPVI(y, X, n_inducing=n_inducing, n_iter=n_iter, thresh=thresh, verbose=verbose, 
+                            use_loader=use_loader, batches=batches, seed=seed)
+    f0.fit()
+
+    f1 = LogisticGPVI(y, X, likelihood=PGLikelihood(), n_inducing=n_inducing, n_iter=n_iter, thresh=thresh, 
+                            verbose=verbose, use_loader=use_loader, batches=batches, seed=seed)
+    f1.fit()
+
+    return torch.tensor([evaluate_method(f0, X_test, y_test), evaluate_method(f1, X_test, y_test)])
+
+
+
+def evaluate_method_application(func, X_test, y_test):
+    y_pred = func.predict(X_test)
+
+    auc = BinaryAUROC()
+    auc.update(y_test, y_pred)
+    auc= auc.compute().item()
+
+    lower, upper = func.credible_intervals(X_test)
+    ci_width = (upper - lower).mean().item()
+
+    return func.runtime, auc, ci_width
+
+
--- src/_04_application.py
+++ src/_04_application.py
@@ -1,46 +1,23 @@
-import os
 import torch
-from _97_gpytorch import LogisticGPVI
 import numpy as np
 import dsdl
-import urllib
-import matplotlib.pyplot as plt
+
+from _00_funcs import process_dataset, evaluate_method_application, analyze_dataset
 
 
-ds = dsdl.load("cod-rna")
-X, y = ds.get_train()
-
-X = X.todense()
-X = torch.tensor(X, dtype=torch.float)
-y = torch.tensor(y, dtype=torch.float)
-
-y[y == -1] = 0
-
-f = LogisticGPVI(y, X, verbose=True, n_iter=350)
-f.fit()
-
-plt.plot(f.loss)
-plt.show()
-
-X_test, y_test = ds.get_test()
-X_test = X_test.todense()
-y_test[y_test == -1] = 0
-
-id1 = np.random.choice(np.where(y_test == 1)[0], 1000, replace=False)
-id0 = np.random.choice(np.where(y_test == 0)[0], 1000, replace=False)
-
-y_test = np.concatenate((y_test[id1], y_test[id0]))
-X_test = X_test[np.concatenate((id1, id0)), :]
-
-y_test = torch.tensor(y_test, dtype=torch.float)
-X_test = torch.tensor(X_test, dtype=torch.float)
-
-y_pred = f.predict(X_test)
+datasets = ["breast-cancer", "diabetes_scale", "phishing", "svmguide1"]
+niters = [200, 200, 100, 20]
+use_loader = [False, False, True, True]
+RUNS = 50
 
 
-import torcheval as tm
+for dataset, niter, loader in zip(datasets, niters, use_loader):
+    y, X, y_test, X_test = process_dataset(dataset)
 
-tm.binary_classification_metrics(y_test, y_pred)
+    res = Parallel(n_jobs=1)(delayed(analyze_dataset)(
+            i, y, X, y_test, X_test, n_iter=niter, use_loader=loader
+        ) for i in range(1, RUNS+1))
 
-torch.sum(y_pred.ge(0.5) == y_test)
-
+    res = torch.stack(res)
+    res = torch.transpose(res, 0, 1)
+    torch.save(res, f"../results/real_data/{dataset}.pt")
