0fd1ea36856a37046bb23e5e1c5f8aac175a8559
mkomod <mkomodromos01@gmail.com>
2024-01-18 16:27:22 +0000

    fix

--- src/_00_funcs.py
+++ src/_00_funcs.py
@@ -131,11 +131,11 @@ def analyze_dataset(seed, y, X, y_test, X_test, n_iter=200, n_inducing=50, thres
     f2.fit()
 
     try:
-        kl_0 = torch.distributions.kl.kl_divergence(f0.model(X), f1.model(X)).item()
-        kl_2 = torch.distributions.kl.kl_divergence(f2.model(X), f1.model(X)).item()
+        kl_0 = torch.distributions.kl.kl_divergence(f1.model(X), f0.model(X)).item()
+        kl_2 = torch.distributions.kl.kl_divergence(f1.model(X), f2.model(X)).item()
 
-        kl_0_test = torch.distributions.kl.kl_divergence(f0.model(X_test), f1.model(X_test)).item()
-        kl_2_test = torch.distributions.kl.kl_divergence(f2.model(X_test), f1.model(X_test)).item()
+        kl_0_test = torch.distributions.kl.kl_divergence(f1.model(X_test), f0.model(X_test)).item()
+        kl_2_test = torch.distributions.kl.kl_divergence(f1.model(X_test), f2.model(X_test)).item()
     except:
         kl_0, kl_2, kl_0_test, kl_2_test = -1.0, -1.0, -1.0, -1.0
 
