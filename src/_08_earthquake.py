1d8f40cfc6b81167678e82832d05774c52daaac7
mkomod <mkomodromos01@gmail.com>
2024-01-18 15:45:51 +0000

    up src

--- src/_08_earthquake.py
+++ src/_08_earthquake.py
@@ -28,11 +28,11 @@ X_train = torch.tensor(X[data.earthquake != quake].values, dtype=torch.double)
 y_train = torch.tensor(y[data.earthquake != quake].values, dtype=torch.double)
 
 # sample 30% of the data for training
-torch.manual_seed(1)
-idx = torch.randperm(X_train.size()[0])
-idx = idx[:int(0.3*X_train.size()[0])]
-X_train = X_train[idx, :]
-y_train = y_train[idx]
+# torch.manual_seed(1)
+# idx = torch.randperm(X_train.size()[0])
+# idx = idx[:int(0.3*X_train.size()[0])]
+# X_train = X_train[idx, :]
+# y_train = y_train[idx]
 
 X_test =  torch.tensor(X[data.earthquake == quake].values, dtype=torch.double)
 y_test =  torch.tensor(y[data.earthquake == quake].values, dtype=torch.double)
@@ -40,36 +40,36 @@ y_test =  torch.tensor(y[data.earthquake == quake].values, dtype=torch.double)
 dat = {"X": X_train, "y": y_train}
 
 # fit the model
-# f0 = LogisticVI(dat, method=0, n_iter=2000, verbose=True, intercept=True, lr=0.03)
-# f0.fit()
-# torch.save(f0, "../results/application/f0.pt")
+f0 = LogisticVI(dat, method=0, n_iter=2000, verbose=True, intercept=True, lr=0.03)
+f0.fit()
+f0.y, f0.X, f0.XX = None, None, None
+torch.save(f0, "../results/application/f0.pt")
+
+f1 = LogisticVI(dat, method=1, n_iter=2000, verbose=True, intercept=True, lr=0.03)
+f1.fit()
+f1.y, f1.X = None, None, None
+torch.save(f1, "../results/application/f1.pt")
+
+f2 = LogisticVI(dat, method=2, n_iter=1000, verbose=True, intercept=True)
+f2.fit()
+f2.y, f2.X = None, None, None
+torch.save(f2, "../results/application/f2.pt")
+
+f3 = LogisticVI(dat, method=3, n_iter=1000, verbose=True, intercept=True)
+f3.fit()
+f2.y, f2.X = None, None, None
+torch.save(f3, "../results/application/f3.pt")
+
+
+# f0 = torch.load("../results/application/f0.pt")
+# f1 = torch.load("../results/application/f1.pt")
+# f2 = torch.load("../results/application/f2.pt")
+# f3 = torch.load("../results/application/f3.pt")
 # 
-# f1 = LogisticVI(dat, method=1, n_iter=2000, verbose=True, intercept=True, lr=0.03)
-# f1.fit()
-# torch.save(f1, "../results/application/f1.pt")
-# 
-# f2 = LogisticVI(dat, method=2, n_iter=1000, verbose=True, intercept=True)
-# f2.fit()
-# torch.save(f2, "../results/application/f2.pt")
-# 
-# f3 = LogisticVI(dat, method=3, n_iter=1000, verbose=True, intercept=True)
-# f3.fit()
-# torch.save(f3, "../results/application/f3.pt")
+# for f in [f0, f1, f2, f3]:
+#     print(f._ELBO_MC(n_samples=20).item())
+#     auc = tm.BinaryAUROC()
+#     auc.update(f.predict(X_test), y_test)
+#     print(auc.compute().item())
 
-f0 = torch.load("../results/application/f0.pt")
-f1 = torch.load("../results/application/f1.pt")
-f2 = torch.load("../results/application/f2.pt")
-f3 = torch.load("../results/application/f3.pt")
-
-for f in [f0, f1, f2, f3]:
-    print(f._ELBO_MC(n_samples=20).item())
-    auc = tm.BinaryAUROC()
-    auc.update(f.predict(X_test), y_test)
-    print(auc.compute().item())
-
-
-f0.m
-f1.m
-f2.m
-f3.m
 
