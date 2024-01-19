6bee2a4a9d5049dadaa780795635fcc4ec0aa7ea
mkomod <mkomodromos01@gmail.com>
2024-01-19 11:42:15 +0000

    up srcW

--- src/_08_earthquake.py
+++ src/_08_earthquake.py
@@ -9,6 +9,8 @@ from _97_gpytorch import LogisticGPVI, LogitLikelihoodMC, PGLikelihood, LogitLik
 
 from torcheval import metrics as tm
 
+from joblib import Parallel, delayed
+
 # qsub -I -l select=1:ncpus=40:mem=80gb -l walltime=08:00:00 
 # export OMP_NUM_THREADS=40
 
@@ -18,47 +20,36 @@ data = pd.read_csv("../data/data_add_std_fillNaNs.csv")
 data.head()
 data.shape
 
-
 y = data.liq
 X = data[["PGV", "Vs30", "precip", "dw", "wtd"]]
-
 quake = "lomaprieta1989"
 
 X_train = torch.tensor(X[data.earthquake != quake].values, dtype=torch.double)
 y_train = torch.tensor(y[data.earthquake != quake].values, dtype=torch.double)
-
-# sample 30% of the data for training
-# torch.manual_seed(1)
-# idx = torch.randperm(X_train.size()[0])
-# idx = idx[:int(0.3*X_train.size()[0])]
-# X_train = X_train[idx, :]
-# y_train = y_train[idx]
-
 X_test =  torch.tensor(X[data.earthquake == quake].values, dtype=torch.double)
 y_test =  torch.tensor(y[data.earthquake == quake].values, dtype=torch.double)
 
 dat = {"X": X_train, "y": y_train}
 
-# fit the model
-f0 = LogisticVI(dat, method=0, n_iter=1000, verbose=True, intercept=True, lr=0.03)
+f0 = LogisticVI(dat, method=0, n_iter=1500, verbose=True, intercept=True, lr=1e-5)
 f0.fit()
 f0.y, f0.X, f0.XX = None, None, None
 torch.save(f0, "../results/application/f0.pt")
 
-f1 = LogisticVI(dat, method=1, n_iter=1000, verbose=True, intercept=True, lr=0.03)
+f1 = LogisticVI(dat, method=1, n_iter=1500, verbose=True, intercept=True, lr=1e-5)
 f1.fit()
 f1.y, f1.X = None, None
 torch.save(f1, "../results/application/f1.pt")
-
-f2 = LogisticVI(dat, method=2, n_iter=1000, verbose=True, intercept=True)
-f2.fit()
-f2.y, f2.X = None, None
-torch.save(f2, "../results/application/f2.pt")
-
-f3 = LogisticVI(dat, method=3, n_iter=1000, verbose=True, intercept=True)
-f3.fit()
-f3.y, f3.X = None, None
-torch.save(f3, "../results/application/f3.pt")
+# 
+# f2 = LogisticVI(dat, method=2, n_iter=1000, verbose=True, intercept=True)
+# f2.fit()
+# f2.y, f2.X = None, None
+# torch.save(f2, "../results/application/f2.pt")
+# 
+# f3 = LogisticVI(dat, method=3, n_iter=1000, verbose=True, intercept=True)
+# f3.fit()
+# f3.y, f3.X = None, None
+# torch.save(f3, "../results/application/f3.pt")
 
 
 # f0 = torch.load("../results/application/f0.pt")
@@ -67,9 +58,22 @@ torch.save(f3, "../results/application/f3.pt")
 # f3 = torch.load("../results/application/f3.pt")
 # 
 # for f in [f0, f1, f2, f3]:
-#     print(f._ELBO_MC(n_samples=20).item())
 #     auc = tm.BinaryAUROC()
 #     auc.update(f.predict(X_test), y_test)
 #     print(auc.compute().item())
-
-
+#     
+#     auc = tm.BinaryAUROC()
+#     auc.update(f.predict(X_train), y_train)
+#     print(auc.compute().item())
+# 
+#     f.X = torch.cat([torch.ones(X_train.size()[0], 1), X_train], dim=1)
+#     f.y = y_train
+#     f.XX = X_train**2
+# 
+#     print(f._ELBO_MC(50))
+# 
+# 
+# torch.round(f0.m, decimals=2)
+# torch.round(f1.m, decimals=2)
+# 
+# f2.m
